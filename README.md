110a1e4814230b41840b3556f81da66ffa32e556
root <root@SensarioHPdev>
2017-06-21 19:47:59 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Dependency document - IoT DevEnv v0.1
+
+This repository is for automating installation of dependencies and programs for my personal development environment through shell scripting. 
+
+## Programs
+
+- Essential utility programs: Zsh, Oh-my-zsh, git, keepass
+- Compilers/interpreters/other for: Golang, Python, GCC ARM, node.js+
+- IDEs with packages: Atom.io for web/python/scripting, Eclipse for bare-metal
+
+#### To be implemented:
+
+- Other utility programs for automation: Ansible, Docker, docker-compose, puppet, jenkins
+- Other SDKs: Google Cloud SDK
+
+## Getting started
+
+Instructions on running the script on your local machine
+
+### Prereq
+
+- 64-bit Debian Linux
+- Java (comes preinstalled)
+
+### Installing
+
+Run the shell script "sudo sh ./install.sh"
+
+## LICENSE
+
+The MIT LICENSE (MIT)
+
+Copyright (c) 2017 Anders L. Hurum 
---
+++ install.sh
@@ -0,0 +1,51 @@
+# Dependency document - IoT DevEnv v0.1
+
+# Essential programs apt-get
+sudo apt-get install zsh git keepass2
+
+# get oh-my-zsh with wget:
+#sudo sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"  &
+echo "add shortcut to open terminal with zsh: gnome-terminal -e zsh"
+
+# Golang
+sudo apt-get golang-go
+
+### Set up gopath
+mkdir -p ~/Workspace/go-workspace
+mkdir -p $HOME/Workspace/go-workspace/src
+mkdir -p $HOME/Workspace/go-workspace/pkg
+mkdir -p $HOME/Workspace/go-workspace/bin
+echo "export GOPATH=$GOPATH:$HOME/Workspace/go-workspace:$HOME/Workspace/go-workspace/bin" >> ~/.bashrc
+source ~/.bashrc
+
+# Python:
+sudo apt-get install python python-pip build-essential python-dev virtualenv 
+
+# Atom io:
+echo "Getting and installing atom.."
+wget https://atom.io/download/deb -O atom.deb
+sudo dpkg -i atom.deb
+
+# Atom packages
+
+### Preview HTML
+apm install atom-html-preview
+### Linter
+apm install linter
+### Python linter
+apm install linter-python
+### File icons
+apm install file-icons
+### Go-plus
+apm install go-plus  
+### Emmet
+apm install emmet  
+
+# Installing and setting up compiler and IDE for ST32
+echo "Proceed to install latest eclipse IDE from their website"
+
+# Next install STLink driver - This is in the repository
+echo "Log in to www.openstm32.org and download stlink_udev_rule.tar.bz2"
+
+# Install 32-bit libraries for Linaro GCC
+sudo apt-get install lib32ncurses5
