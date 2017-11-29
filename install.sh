509e3c218b03d9ad0907b1ca6343a3a8d9761a9d
peakbreaker <peakbreaker@peakbreaker.peakbreaker.com>
2017-11-29 14:44:44 +0100

    fix: Fixed various issues with install script
    Tried the install scripts on new computer and found some faults, so fixed various things
    - Removed the appending of sources things (so that running the script multiple times doesnt cause issues)
    - Changes in how sudo is run

--- install.sh
+++ install.sh
@@ -1,49 +1,39 @@
-# Dependency document - IoT DevEnv v0.1
-
 # Essential programs apt-get
-sudo apt-get install zsh git keepass2 curl wget
+sudo apt install -y zsh git keepass2 curl wget
 
 # get oh-my-zsh with wget:
-sudo sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"  
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"  
 
-# Golang
-sudo apt install golang-go
+# Golang 
+sudo apt install -y golang-go
 
 ### Set up gopath
 mkdir -p ~/Workspace/go-workspace
 mkdir -p $HOME/Workspace/go-workspace/src
 mkdir -p $HOME/Workspace/go-workspace/pkg
 mkdir -p $HOME/Workspace/go-workspace/bin
-echo "export GOPATH=$GOPATH:$HOME/Workspace/go-workspace:$HOME/Workspace/go-workspace/bin" >> ~/.bashrc
+#echo "export GOPATH=$GOPATH:$HOME/Workspace/go-workspace:$HOME/Workspace/go-workspace/bin" >> ~/.bashrc
 source ~/.bashrc
 
 # SQLite
-sudo apt-get install rlwrap sqlite3 socat
+sudo apt install -y rlwrap sqlite3 socat
 
 # Python:
-sudo apt-get install python python-pip build-essential python-dev virtualenv
+sudo apt install -y python python-pip build-essential python-dev virtualenv
 
 # Ansible:
-sudo apt-get install ansible sshpass
+sudo apt install -y ansible sshpass
 
 # Virtualbox and Vagrant:
 ## First add to sources list:
-sudo ($echo "deb http://download.virtualbox.org/virtualbox/debian stretch contrib") >> /etc/apt/sources.list.d
+#sudo ($echo "deb http://download.virtualbox.org/virtualbox/debian stretch contrib") >> /etc/apt/sources.list.d
 ## Next we add oracle public key
-curl -O https://www.virtualbox.org/download/oracle_vbox_2016.asc
-sudo apt-key add oracle_vbox_2016.asc
+#curl -O https://www.virtualbox.org/download/oracle_vbox_2016.asc
+#sudo apt-key add oracle_vbox_2016.asc
 ## Finally we install the programs
-sudo apt-get update
-sudo apt-get install virtualbox-5.1
-sudo apt-get install vagrant
-
-# Ansible
-## First we add to sources list
-sudo ($echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main") >> /etc/apt/sources.list.d
-
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-sudo apt-get update
-sudo apt install ansible
+#sudo apt-get update
+#sudo apt-get install virtualbox-5.1
+#sudo apt-get install vagrant
 
 # Atom io:
 echo "Getting and installing atom.."
@@ -69,8 +59,13 @@ apm install emmet
 # sudo apt-get install lib32ncurses5
 
 # Install nodejs and npm
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt install -y nodejs
+
+# Remove the atom.deb and notify user of end of installs
+rm atom.deb
+
+echo ""
+echo " --------- END OF AUTOMATIC INSTALLS ---------"
 
 # Installing and setting up compiler and IDE for ST32
 echo "Proceed to install latest eclipse IDE from their website"
